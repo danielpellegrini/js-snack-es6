c8ce73f3c11fb38f913027aaf1c86f7c2935f5dc
Daniel <danielpellegrini87@gmail.com>
2021-01-20 17:26:35 +0100

    js-snack-3-es6-completed
    changed VAR con CONST

--- es-6/snack-3/js/script.js
+++ es-6/snack-3/js/script.js
@@ -1,10 +1,10 @@
 // jsnack 3
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
-var numbersArray=[0,1,2,3,4,5,6,7,8,9,10];
+const numbersArray=[0,1,2,3,4,5,6,7,8,9,10];
 
 const filteredArray = numbersArray.filter((element, index, array) => {
-  
+
   return element > 2 && element < 8 ;
 
 });
